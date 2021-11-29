3738f4d0e77fc33fea33dd84ed81013d65c6b09f
Joanna Mazur <joamaz@st.amu.edu.pl>
2021-11-29 19:24:19 +0100

    Export more than 2 hierarchy levels

--- File Hierarchy.js
+++ File Hierarchy.js
@@ -16,72 +16,74 @@
   "browserSupport": "gcsv",
   "priority": 100,
   "inRepository": false,
-  "lastUpdated": "2018-09-28 15:58:41"
+  "lastUpdated": "2021-11-29 19:18:51"
 }
 
 class Collections {
-    constructor() {
-        this.collections = {};
-        this.saved = {};
-        let coll;
-        while (coll = Zotero.nextCollection()) {
-            const key = (coll.primary ? coll.primary : coll).key;
-            this.collections[key] = {
-                parent: coll.fields.parentKey,
-                name: coll.name,
-            };
-        }
-        for (const key in this.collections) {
-            const coll = this.collections[key];
-            if (coll.parent && !this.collections[coll.parent]) {
-                coll.parent = false;
-            }
-            coll.path = this.path(coll);
-        }
-        Zotero.debug('collections: ' + JSON.stringify(this.collections));
-    }
-    clean(filename) {
-        return filename.replace(/[#%&{}\\<>\*\?\/\$!'":@]/g, '_');
-    }
-    path(coll) {
-        return (this.collections[coll.parent] ? this.path(this.collections[coll.parent]) : '') + this.clean(coll.name) + '/';
-    }
-    save(item) {
-        const attachments = (item.itemType === 'attachment') ? [item] : (item.attachments || []);
-        const collections = (item.collections || []).map(key => this.collections[key]).filter(coll => coll);
-        for (const att of attachments) {
-            if (!att.defaultPath)
-                continue;
-            const subdir = [
-                // (item.itemType !== 'attachment' ? this.clean(item.title) : null),
-                /* assume text/html is snapshot */
-                (att.contentType === 'text/html' ? this.clean(att.filename.replace(/\.html?$/, '')) : null),
-            ].filter(p => p).join('/');
-            for (const coll of (collections.length ? collections : [{ path: '' }])) {
-                const path = `${coll.path}${subdir}`;
-                this.saved[path] = this.saved[path] || {};
-                const parts = att.filename.split('.');
-                const ext = this.clean(parts.length > 1 ? ('.' + parts.pop()) : '');
-                const basename = this.clean(parts.join('.'));
-                let postfix = 0;
-                let filename = basename + ext;
-                while (this.saved[filename]) {
-                    filename = `${basename}_${++postfix}${ext}`;
+  constructor() {
+    this.collections = {};
+    let coll;
+    let path;
+    let childp;
+    while (coll = Zotero.nextCollection()) {
+      for (const e of coll.descendents) {
+        path = coll.fields.name + '/' + e.name;
+        this.collections[e.key] = {
+          path: path,
+        };
+        if (e.children) {
+          for (const c of e.children) {
+            childp=path;
+            if (c.type == 'collection') {
+              childp = childp + '/' + c.name;
+              this.collections[c.key] = {
+                  path: childp,
+              };
+              if (c.children) {
+                for (const c2 of c.children) {
+                  if (c2.type == 'collection') {
+                    childp = childp + '/' + c2.name;
+                    this.collections[c2.key] = {
+                        path: childp,
+                    };
+                  }
                 }
-                this.saved[path][filename] = true;
-                Zotero.debug(`saving to ${path}/${filename}\n`);
-                att.saveFile(`${path}/${filename}`, true);
-                Zotero.write(`${path}/${filename}\n`);
+              }
             }
+          }
         }
+      }
+      this.collections[coll.primary.key] = {
+          path: path,
+      };
     }
+    Zotero.debug('coll :' + JSON.stringify(this.collections));
+  }
+  clean(filename) {
+      return filename.replace(/[#%&{}\\<>\*\?\/\$!'":@]/g, '_');
+  }
+  save(item) {
+    const attachments = (item.itemType === 'attachment') ? [item] : (item.attachments || []);
+    const collections = (item.collections || []).map(key => this.collections[key]).filter(coll => coll);
+    for (const att of attachments) {
+      if (!att.defaultPath || att.contentType === 'text/html' || !att.filename)
+          continue;
+      for (const coll of (collections.length ? collections : [{ path: '' }])) {
+        if (coll.path == '')
+          Zotero.debug('item :' + JSON.stringify(item));
+
+        att.saveFile(coll.path + '/' + att.filename, true);
+      }
+    }
+  }
 }
+
 function doExport() {
-    if (!Zotero.getOption('exportFileData'))
-        throw new Error('File Hierarchy needs "Export File Data" to be on');
-    const collections = new Collections;
-    let item, attachments;
-    while ((item = Zotero.nextItem())) {
-        collections.save(item);
-    }
+  if (!Zotero.getOption('exportFileData'))
+    throw new Error('File Hierarchy needs "Export File Data" to be on');
+  const collections = new Collections;
+  let item, attachments;
+  while ((item = Zotero.nextItem())) {
+    collections.save(item);
+  }
 }
